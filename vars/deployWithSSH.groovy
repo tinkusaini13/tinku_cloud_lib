b3cd7a8304381bf85c2e049004e181ce9d2dc1f8
Tinku Saini <88707521+tinkusaini13@users.noreply.github.com>
2024-01-08 23:59:46 +0530

    Create deployWithSSH.groovy

---
+++ vars/deployWithSSH.groovy
@@ -0,0 +1,20 @@
+def call(String credentialsId) {
+    def serverHost = '15.206.163.197'
+    def serverPort = '22'
+    def username = 'tomcat'
+    def remotePath = '/tmp/'
+
+    stage('Deploy') {
+        sshDeploy(serverHost, serverPort, username, remotePath, credentialsId)
+    }
+}
+
+def sshDeploy(String serverHost, String serverPort, String username, String remotePath, String credentialsId) {
+    script {
+        withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')])  // Obtain the password from Jenkins credentials
+        def password = credentials(credentialsId)
+        // Your SSH deployment steps here
+        echo "Deploying to ${serverHost}:${serverPort} with username ${username} and password *** (masked)"
+        sh "sshpass -p ${password} scp -P ${serverPort} *.war ${username}@${serverHost}:${remotePath}"
+    }
+}
